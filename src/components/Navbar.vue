a385b2ba4e25bec87828508057d943e04266806f
Nabila-salsabiya <salsabiyawak18@icloud.com>
2026-09-25 07:38:38 +0700

    pushh

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,38 @@
 <template>
- <nav>
- <h2>Nabila salsabila - OnlineShop</h2>
- <RouterLink to="/">Landing</RouterLink> |
- <RouterLink to="/product">Product</RouterLink> |
- <RouterLink to="/about">About</RouterLink> |
- <RouterLink to="/contact">Contact</RouterLink> |
- </nav>
- <hr />
-</template>
+  <nav class="w-full shadow-md flex ">
+    <!-- <div class=""> -->
+      <p class="flex justify-start text-3xl ">Nabila-olshop</p>
+      <!-- <div class="jus gap-10 py-4 justify-between">  -->
+        <div class="flex justify-end items-center gap-10 py-4"> 
+          
+      <RouterLink
+        to="/"
+        class="text-gray-700 font-medium hover:text-pink-500"
+      >
+        Landing
+      </RouterLink>
 
+      <RouterLink
+        to="/product"
+        class="text-gray-700 font-medium hover:text-pink-500"
+      >
+        Product
+      </RouterLink>
+
+      <RouterLink
+        to="/about"
+        class="text-gray-700 font-medium hover:text-pink-500"
+      >
+        About
+      </RouterLink>
+
+      <RouterLink
+        to="/contact"
+        class="text-gray-700 font-medium hover:text-pink-500"
+      >
+        Contact
+      </RouterLink>
+
+    </div>
+  </nav>
+</template>
